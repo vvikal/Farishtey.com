9643ca47fea406043eb14bfa1a473294117a2876
vvikal <45101781+vvikal@users.noreply.github.com>
2020-02-09 15:44:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	$(window).scroll(function() {
+		if ($(document).scrollTop() > 10) {
+			$('#nav').addClass('shrink');
+		} else {
+			$('#nav').removeClass('shrink');
+		}
+	});
+});
